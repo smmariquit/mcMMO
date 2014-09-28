Pershonkey/McMMOPlus

UUID support for parties

src/main/java/com/gmail/nossr50/api/PartyAPI.java
@@ -1,14 +1,17 @@
 package com.gmail.nossr50.api;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.UUID;
 
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 import com.gmail.nossr50.mcMMO;
 import com.gmail.nossr50.datatypes.party.Party;
+import com.gmail.nossr50.datatypes.party.PartyLeader;
 import com.gmail.nossr50.party.PartyManager;
 import com.gmail.nossr50.util.player.UserManager;
 
@@ -79,7 +82,7 @@ public final class PartyAPI {
         Party party = PartyManager.getParty(partyName);
 
         if (party == null) {
-            party = new Party(player.getName(), partyName);
+            party = new Party(new PartyLeader(player.getName(), player.getUniqueId()), partyName);
         }
 
         PartyManager.addToParty(UserManager.getPlayer(player), party);
@@ -105,7 +108,7 @@ public final class PartyAPI {
      * @return the leader of the party
      */
     public static String getPartyLeader(String partyName) {
-        return PartyManager.getPartyLeader(partyName);
+        return PartyManager.getPartyLeaderName(partyName);
     }
 
     /**
@@ -114,10 +117,11 @@ public final class PartyAPI {
      * This function is designed for API usage.
      *
      * @param partyName The name of the party to set the leader of
-     * @param player The player to set as leader
+     * @param playerName The playerName to set as leader
      */
-    public static void setPartyLeader(String partyName, String player) {
-        PartyManager.setPartyLeader(player, PartyManager.getParty(partyName));
+    @Deprecated
+    public static void setPartyLeader(String partyName, String playerName) {
+        PartyManager.setPartyLeader(mcMMO.p.getServer().getOfflinePlayer(playerName).getUniqueId(), PartyManager.getParty(partyName));
     }
 
     /**
@@ -132,8 +136,8 @@ public final class PartyAPI {
     public static List<OfflinePlayer> getOnlineAndOfflineMembers(Player player) {
         List<OfflinePlayer> members = new ArrayList<OfflinePlayer>();
 
-        for (String memberName : PartyManager.getAllMembers(player)) {
-            OfflinePlayer member = mcMMO.p.getServer().getOfflinePlayer(memberName);
+        for (UUID memberUniqueId : PartyManager.getAllMembers(player).values()) {
+            OfflinePlayer member = mcMMO.p.getServer().getOfflinePlayer(memberUniqueId);
             members.add(member);
         }
         return members;
@@ -147,7 +151,20 @@ public final class PartyAPI {
      * @param player The player to check
      * @return all the player names in the player's party
      */
+    @Deprecated
     public static LinkedHashSet<String> getMembers(Player player) {
+        return (LinkedHashSet<String>) PartyManager.getAllMembers(player).keySet();
+    }
+
+    /**
+     * Get a list of all player names and uuids in this player's party.
+     * </br>
+     * This function is designed for API usage.
+     *
+     * @param player The player to check
+     * @return all the player names and uuids in the player's party
+     */
+    public static LinkedHashMap<String, UUID> getMembersMap(Player player) {
         return PartyManager.getAllMembers(player);
     }
 

src/main/java/com/gmail/nossr50/datatypes/party/Party.java
@@ -2,8 +2,10 @@ package com.gmail.nossr50.datatypes.party;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.UUID;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
@@ -20,9 +22,9 @@ import com.gmail.nossr50.util.EventUtils;
 import com.gmail.nossr50.util.Misc;
 
 public class Party {
-    private final LinkedHashSet<String> members = new LinkedHashSet<String>();
+    private final LinkedHashMap<String, UUID> members = new LinkedHashMap<String, UUID>();
 
-    private String leader;
+    private PartyLeader leader;
     private String name;
     private String password;
     private boolean locked;
@@ -43,14 +45,14 @@ public class Party {
         this.name = name;
     }
 
-    public Party(String leader, String name) {
+    public Party(PartyLeader leader, String name) {
         this.leader = leader;
         this.name = name;
         this.locked = true;
         this.level = 0;
     }
 
-    public Party(String leader, String name, String password) {
+    public Party(PartyLeader leader, String name, String password) {
         this.leader = leader;
         this.name = name;
         this.password = password;
@@ -58,7 +60,7 @@ public class Party {
         this.level = 0;
     }
 
-    public Party(String leader, String name, String password, boolean locked) {
+    public Party(PartyLeader leader, String name, String password, boolean locked) {
         this.leader = leader;
         this.name = name;
         this.password = password;
@@ -66,15 +68,15 @@ public class Party {
         this.level = 0;
     }
 
-    public LinkedHashSet<String> getMembers() {
+    public LinkedHashMap<String, UUID> getMembers() {
         return members;
     }
 
     public List<Player> getOnlineMembers() {
         List<Player> onlineMembers = new ArrayList<Player>();
 
-        for (String memberName : members) {
-            Player member = mcMMO.p.getServer().getPlayerExact(memberName);
+        for (UUID uuid : members.values()) {
+            Player member = mcMMO.p.getServer().getPlayer(uuid);
 
             if (member != null) {
                 onlineMembers.add(member);
@@ -101,7 +103,7 @@ public class Party {
         return name;
     }
 
-    public String getLeader() {
+    public PartyLeader getLeader() {
         return leader;
     }
 
@@ -133,7 +135,7 @@ public class Party {
         this.name = name;
     }
 
-    public void setLeader(String leader) {
+    public void setLeader(PartyLeader leader) {
         this.leader = leader;
     }
 
@@ -220,7 +222,8 @@ public class Party {
         }
 
         if (!Config.getInstance().getPartyInformAllMembers()) {
-            Player leader = mcMMO.p.getServer().getPlayer(this.leader);
+            Player leader = mcMMO.p.getServer().getPlayer(this.leader.getUniqueId());
+
             if (leader != null) {
                 leader.sendMessage(LocaleLoader.getString("Party.LevelUp", levelsGained, getLevel()));
 
@@ -306,10 +309,13 @@ public class Party {
     public String createMembersList(String playerName, List<Player> nearMembers) {
         StringBuilder memberList = new StringBuilder();
 
-        for (String memberName : this.getMembers()) {
-            Player member = mcMMO.p.getServer().getPlayerExact(memberName);
+        for (Entry<String, UUID> memberEntry : this.getMembers().entrySet()) {
+            String memberName = memberEntry.getKey();
+            UUID uuid = memberEntry.getValue();
 
-            if (this.getLeader().equalsIgnoreCase(memberName)) {
+            Player member = mcMMO.p.getServer().getPlayer(uuid);
+
+            if (this.getLeader().getUniqueId().equals(uuid)) {
                 memberList.append(ChatColor.GOLD);
 
                 if (member == null) {

src/main/java/com/gmail/nossr50/runnables/party/PartyAutoKickTask.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.UUID;
 
 import org.bukkit.OfflinePlayer;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -19,21 +20,21 @@ public class PartyAutoKickTask extends BukkitRunnable {
     @Override
     public void run() {
         HashMap<OfflinePlayer, Party> toRemove = new HashMap<OfflinePlayer, Party>();
-        List<String> processedPlayers = new ArrayList<String>();
+        List<UUID> processedPlayers = new ArrayList<UUID>();
 
         long currentTime = System.currentTimeMillis();
 
         for (Party party : PartyManager.getParties()) {
-            for (String memberName : party.getMembers()) {
-                OfflinePlayer member = mcMMO.p.getServer().getOfflinePlayer(memberName);
-                boolean isProcessed = processedPlayers.contains(memberName);
+            for (UUID memberUniqueId : party.getMembers().values()) {
+                OfflinePlayer member = mcMMO.p.getServer().getOfflinePlayer(memberUniqueId);
+                boolean isProcessed = processedPlayers.contains(memberUniqueId);
 
                 if ((!member.isOnline() && (currentTime - member.getLastPlayed() > KICK_TIME)) || isProcessed) {
                     toRemove.put(member, party);
                 }
 
                 if (!isProcessed) {
-                    processedPlayers.add(memberName);
+                    processedPlayers.add(memberUniqueId);
                 }
             }
         }

smmariquit/mcMMO

src/main/java/com/gmail/nossr50/api/PartyAPI.java
@@ -1,14 +1,17 @@
 package com.gmail.nossr50.api;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.UUID;
 
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 import com.gmail.nossr50.mcMMO;
 import com.gmail.nossr50.datatypes.party.Party;
+import com.gmail.nossr50.datatypes.party.PartyLeader;
 import com.gmail.nossr50.party.PartyManager;
 import com.gmail.nossr50.util.player.UserManager;
 
@@ -79,7 +82,7 @@ public final class PartyAPI {
         Party party = PartyManager.getParty(partyName);
 
         if (party == null) {
-            party = new Party(player.getName(), partyName);
+            party = new Party(new PartyLeader(player.getUniqueId(), player.getName()), partyName);
         }
 
         PartyManager.addToParty(UserManager.getPlayer(player), party);
@@ -105,7 +108,7 @@ public final class PartyAPI {
      * @return the leader of the party
      */
     public static String getPartyLeader(String partyName) {
-        return PartyManager.getPartyLeader(partyName);
+        return PartyManager.getPartyLeaderName(partyName);
     }
 
     /**
@@ -114,10 +117,11 @@ public final class PartyAPI {
      * This function is designed for API usage.
      *
      * @param partyName The name of the party to set the leader of
-     * @param player The player to set as leader
+     * @param playerName The playerName to set as leader
      */
-    public static void setPartyLeader(String partyName, String player) {
-        PartyManager.setPartyLeader(player, PartyManager.getParty(partyName));
+    @Deprecated
+    public static void setPartyLeader(String partyName, String playerName) {
+        PartyManager.setPartyLeader(mcMMO.p.getServer().getOfflinePlayer(playerName).getUniqueId(), PartyManager.getParty(partyName));
     }
 
     /**
@@ -132,8 +136,8 @@ public final class PartyAPI {
     public static List<OfflinePlayer> getOnlineAndOfflineMembers(Player player) {
         List<OfflinePlayer> members = new ArrayList<OfflinePlayer>();
 
-        for (String memberName : PartyManager.getAllMembers(player)) {
-            OfflinePlayer member = mcMMO.p.getServer().getOfflinePlayer(memberName);
+        for (UUID memberUniqueId : PartyManager.getAllMembers(player).keySet()) {
+            OfflinePlayer member = mcMMO.p.getServer().getOfflinePlayer(memberUniqueId);
             members.add(member);
         }
         return members;
@@ -147,7 +151,20 @@ public final class PartyAPI {
      * @param player The player to check
      * @return all the player names in the player's party
      */
+    @Deprecated
     public static LinkedHashSet<String> getMembers(Player player) {
+        return (LinkedHashSet<String>) PartyManager.getAllMembers(player).values();
+    }
+
+    /**
+     * Get a list of all player names and uuids in this player's party.
+     * </br>
+     * This function is designed for API usage.
+     *
+     * @param player The player to check
+     * @return all the player names and uuids in the player's party
+     */
+    public static LinkedHashMap<UUID, String> getMembersMap(Player player) {
         return PartyManager.getAllMembers(player);
     }
 

src/main/java/com/gmail/nossr50/datatypes/party/Party.java
@@ -2,8 +2,10 @@ package com.gmail.nossr50.datatypes.party;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.UUID;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
@@ -20,9 +22,10 @@ import com.gmail.nossr50.util.EventUtils;
 import com.gmail.nossr50.util.Misc;
 
 public class Party {
-    private final LinkedHashSet<String> members = new LinkedHashSet<String>();
+    private final LinkedHashMap<UUID, String> members = new LinkedHashMap<UUID, String>();
+    private final List<Player> onlineMembers = new ArrayList<Player>();
 
-    private String leader;
+    private PartyLeader leader;
     private String name;
     private String password;
     private boolean locked;
@@ -43,14 +46,14 @@ public class Party {
         this.name = name;
     }
 
-    public Party(String leader, String name) {
+    public Party(PartyLeader leader, String name) {
         this.leader = leader;
         this.name = name;
         this.locked = true;
         this.level = 0;
     }
 
-    public Party(String leader, String name, String password) {
+    public Party(PartyLeader leader, String name, String password) {
         this.leader = leader;
         this.name = name;
         this.password = password;
@@ -58,7 +61,7 @@ public class Party {
         this.level = 0;
     }
 
-    public Party(String leader, String name, String password, boolean locked) {
+    public Party(PartyLeader leader, String name, String password, boolean locked) {
         this.leader = leader;
         this.name = name;
         this.password = password;
@@ -66,21 +69,11 @@ public class Party {
         this.level = 0;
     }
 
-    public LinkedHashSet<String> getMembers() {
+    public LinkedHashMap<UUID, String> getMembers() {
         return members;
     }
 
     public List<Player> getOnlineMembers() {
-        List<Player> onlineMembers = new ArrayList<Player>();
-
-        for (String memberName : members) {
-            Player member = mcMMO.p.getServer().getPlayerExact(memberName);
-
-            if (member != null) {
-                onlineMembers.add(member);
-            }
-        }
-
         return onlineMembers;
     }
 
@@ -97,11 +90,19 @@ public class Party {
         return onlinePlayerNames;
     }
 
+    public boolean addOnlineMember(Player player) {
+        return onlineMembers.add(player);
+    }
+
+    public boolean removeOnlineMember(Player player) {
+        return onlineMembers.remove(player);
+    }
+
     public String getName() {
         return name;
     }
 
-    public String getLeader() {
+    public PartyLeader getLeader() {
         return leader;
     }
 
@@ -133,7 +134,7 @@ public class Party {
         this.name = name;
     }
 
-    public void setLeader(String leader) {
+    public void setLeader(PartyLeader leader) {
         this.leader = leader;
     }
 
@@ -220,7 +221,8 @@ public class Party {
         }
 
         if (!Config.getInstance().getPartyInformAllMembers()) {
-            Player leader = mcMMO.p.getServer().getPlayer(this.leader);
+            Player leader = mcMMO.p.getServer().getPlayer(this.leader.getUniqueId());
+
             if (leader != null) {
                 leader.sendMessage(LocaleLoader.getString("Party.LevelUp", levelsGained, getLevel()));
 
@@ -303,13 +305,24 @@ public class Party {
         }
     }
 
+    public boolean hasMember(String memberName) {
+        return this.getMembers().keySet().contains(memberName);
+    }
+
+    public boolean hasMember(UUID uuid) {
+        return this.getMembers().values().contains(uuid);
+    }
+
     public String createMembersList(String playerName, List<Player> nearMembers) {
         StringBuilder memberList = new StringBuilder();
 
-        for (String memberName : this.getMembers()) {
-            Player member = mcMMO.p.getServer().getPlayerExact(memberName);
+        for (Entry<UUID, String> memberEntry : this.getMembers().entrySet()) {
+            UUID uuid = memberEntry.getKey();
+            String memberName = memberEntry.getValue();
 
-            if (this.getLeader().equalsIgnoreCase(memberName)) {
+            Player member = mcMMO.p.getServer().getPlayer(uuid);
+
+            if (this.getLeader().getUniqueId().equals(uuid)) {
                 memberList.append(ChatColor.GOLD);
 
                 if (member == null) {

src/main/java/com/gmail/nossr50/runnables/party/PartyAutoKickTask.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.UUID;
 
 import org.bukkit.OfflinePlayer;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -19,21 +20,21 @@ public class PartyAutoKickTask extends BukkitRunnable {
     @Override
     public void run() {
         HashMap<OfflinePlayer, Party> toRemove = new HashMap<OfflinePlayer, Party>();
-        List<String> processedPlayers = new ArrayList<String>();
+        List<UUID> processedPlayers = new ArrayList<UUID>();
 
         long currentTime = System.currentTimeMillis();
 
         for (Party party : PartyManager.getParties()) {
-            for (String memberName : party.getMembers()) {
-                OfflinePlayer member = mcMMO.p.getServer().getOfflinePlayer(memberName);
-                boolean isProcessed = processedPlayers.contains(memberName);
+            for (UUID memberUniqueId : party.getMembers().keySet()) {
+                OfflinePlayer member = mcMMO.p.getServer().getOfflinePlayer(memberUniqueId);
+                boolean isProcessed = processedPlayers.contains(memberUniqueId);
 
                 if ((!member.isOnline() && (currentTime - member.getLastPlayed() > KICK_TIME)) || isProcessed) {
                     toRemove.put(member, party);
                 }
 
                 if (!isProcessed) {
-                    processedPlayers.add(memberName);
+                    processedPlayers.add(memberUniqueId);
                 }
             }
         }